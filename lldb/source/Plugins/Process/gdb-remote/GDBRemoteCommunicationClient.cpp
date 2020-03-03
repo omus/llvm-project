eaebd73d49ce137197a747550facbfa0e0db19da
Hans Wennborg <hans@chromium.org>
2020-03-03 09:45:14 +0100

    Revert abb00753 "build: reduce CMake handling for zlib" (PR44780)
    and follow-ups:
    a2ca1c2d "build: disable zlib by default on Windows"
    2181bf40 "[CMake] Link against ZLIB::ZLIB"
    1079c68a "Attempt to fix ZLIB CMake logic on Windows"
    
    This changed the output of llvm-config --system-libs, and more
    importantly it broke stand-alone builds. Instead of piling on more fix
    attempts, let's revert this to reduce the risk of more breakages.
    
    (cherry picked from commit 916be8fd6a0a0feea4cefcbeb0c22c65848d7a2e)

--- lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
+++ lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
@@ -1046,7 +1046,7 @@ void GDBRemoteCommunicationClient::MaybeEnableCompression(
   }
 #endif
 
-#if LLVM_ENABLE_ZLIB
+#if defined(HAVE_LIBZ)
   if (avail_type == CompressionType::None) {
     for (auto compression : supported_compressions) {
       if (compression == "zlib-deflate") {
