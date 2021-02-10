ebfe64894081c55d844ef6cbdbde5b738688ab3c
Curtis Vogt <curtis.vogt@gmail.com>
2021-02-10 08:21:11 -0600

    Revert "[X86] Move frontend CPU feature initialization to a look up table based implementation. NFCI"
    This reverts commit 3537939cda86f0b5b06233eb99ddc9eb22935008.

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -11707,7 +11707,7 @@ CodeGenFunction::GetX86CpuSupportsMask(ArrayRef<StringRef> FeatureStrs) {
   for (const StringRef &FeatureStr : FeatureStrs) {
     unsigned Feature =
         StringSwitch<unsigned>(FeatureStr)
-#define X86_FEATURE_COMPAT(ENUM, STR) .Case(STR, llvm::X86::FEATURE_##ENUM)
+#define X86_FEATURE_COMPAT(ENUM, STR) .Case(STR, llvm::X86::ENUM)
 #include "llvm/Support/X86TargetParser.def"
         ;
     FeaturesMask |= (1ULL << Feature);
